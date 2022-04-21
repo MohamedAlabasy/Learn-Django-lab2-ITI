2568d6d28a05b43963dc3e37d9283af287b09bb6
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-04-22 01:11:20 +0200

    finish task

--- books/models.py
+++ books/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=250)
+    details = models.CharField(max_length=250, null=True)
+    image = models.URLField(null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=250)
+    summary = models.CharField(max_length=250)
+    publish_date = models.DateTimeField(auto_now_add=True)
+    image = models.URLField()
+    add_to_site = models.DateTimeField(default=timezone.now)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    appropriate = models.CharField(max_length=250, choices=[(
+        "und", "under 18"), ("ad", "Adults")], default="ad")
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        # return str(self.item) + ": $" + str(self.price)
+        return self.name
--- books/views.py
+++ books/views.py
@@ -1,31 +1,36 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Book, Author
 # Create your views here.
 
 
 def list_books(request):
+    all_books = Book.objects.all()
+    authors = all_books.prefetch_related('author')
     context = {
-        "data": "data"
+        "books": all_books,
+        "author": authors
     }
+    print(authors)
     return render(request, 'books/list_books.html', context=context)
 
 
 def show_book(request, book_id):
     context = {
-        "data": book_id
+        "book": Book.objects.get(id=book_id),
     }
     return render(request, 'books/show_book.html', context=context)
 
 
 def list_authors(request):
     context = {
-        "data": "data"
+        "authors":  Author.objects.all(),
     }
     return render(request, 'books/list_authors.html', context=context)
 
 
 def show_author(request, author_id):
     context = {
-        "data": author_id
+        "author": Author.objects.get(id=author_id),
+        "books": Book.objects.filter(author_id=author_id).all(),
     }
     return render(request, 'books/show_author.html', context=context)
